8181d7a9b797195372563b7eddbda0c1c94e52eb
tsukaryu <drgon8otsuka@gmail.com>
2024-07-15 12:31:00 +0900

    fix: コメント追加

---
+++ page-list.mjs
@@ -0,0 +1,12 @@
+export const PAGES = [
+  "/_app.tsx",
+  "/_document.tsx",
+  "/api/hello.ts",
+  "/fuga/confirm/[confirmId]",
+  "/fuga/create",
+  "/fuga/edit/[someId]",
+  "/hoge/confirm/[id]",
+  "/hoge/create",
+  "/hoge/edit/[id]",
+  "/",
+];
